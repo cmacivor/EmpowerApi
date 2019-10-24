10fa775b642fa4207963d943525b658a0059cc25
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2019-10-24 17:13:07 -0400

    add service outcome controller

---
+++ EmpowerApi/Controllers/ServiceOutcomeController.cs
@@ -0,0 +1,65 @@
+﻿using DJSCaseMgmtModel.Entities;
+using DJSCaseMgtService.DataAccess.Repositories;
+using DJSCaseMgtService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmpowerApi.Controllers
+{
+    [RoutePrefix("api/ServiceOutcome")]
+    public class ServiceOutcomeController : BaseController<ServiceOutcome>
+    {
+        private DJSCaseMgtContext context = new DJSCaseMgtContext();
+
+        public ServiceOutcomeController(IBaseRepository<ServiceOutcome> context) : base(context)
+        {
+        }
+
+        [System.Web.Http.HttpGet, Route("GetAll")]
+        public IHttpActionResult GetAll()
+        {
+            int systemID = base.authRepository.GetSystemIDByLoggedInUserRole();
+
+            IEnumerable<ServiceOutcome> output = null;
+            if (ModelState.IsValid)
+            {
+                output = context.ServiceOutcome.Where(x => x.Active == true && x.SystemID == systemID).ToList();
+            }
+
+            return Ok(output);
+        }
+
+
+        [HttpGet, Route("Delete/{id:int}")]
+        public IHttpActionResult Delete(int id)
+        {
+            string result = "";
+            if (ModelState.IsValid)
+            {
+                var record = context.ServiceOutcome.Where(x => x.ID == id).FirstOrDefault();
+                if (record != null)
+                {
+                    context.ServiceOutcome.Remove(record);
+                }
+                try
+                {
+                    context.SaveChanges();
+                    result = "s";
+                    return Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    result = "f";
+                    return Ok(result);
+                }
+            }
+
+            return Ok(result);
+        }
+
+    }
+}
