4b96c77b4d576826bbf79fc7715a5c094aacdc8e
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2019-12-19 16:42:47 -0500

    add client profile

---
+++ EmpowerApi/Controllers/ClientProfileController.cs
@@ -0,0 +1,30 @@
+﻿using DJSCaseMgmtModel.ViewModels;
+using DJSCaseMgtService.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmpowerApi.Controllers
+{
+    [RoutePrefix("api/ClientProfile")]
+    public class ClientProfileController : ApiController
+    {
+        private IClientProfileRepository _clientProfileRepository;
+
+        public ClientProfileController(IClientProfileRepository clientProfileRepository)
+        {
+            _clientProfileRepository = clientProfileRepository;
+        }
+
+
+        [System.Web.Http.HttpPost, Route("Search")]
+        public IEnumerable<ClientSearchResults> Search(ClientName name)
+        {
+            var retVal = _clientProfileRepository.Search(name.LastName, name.FirstName);
+            return retVal;
+        }
+    }
+}
