5525c51746a56ebc00322e179048bc6ccdcafa70
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2020-01-13 10:43:30 -0500

    add gender controller

---
+++ EmpowerApi/Controllers/GenderController.cs
@@ -0,0 +1,17 @@
+﻿using DJSCaseMgmtModel.Entities;
+using DJSCaseMgtService.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmpowerApi.Controllers
+{
+    [RoutePrefix("api/Gender")]
+    public class GenderController : BaseController<Gender>
+    {
+        public GenderController(IBaseRepository<Gender> baseRepository) : base(baseRepository) { }
+    }
+}
