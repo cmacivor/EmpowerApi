750254876d5f6c9be5ad1e5fc59e0db4610cc60e
Craig.MacIvor <Craig.MacIvor@richmondgov.com>
2020-01-31 16:11:07 -0500

    service proflie

---
+++ EmpowerApi/Controllers/DocumentController.cs
@@ -0,0 +1,61 @@
+﻿using DJSCaseMgmtModel.Entities;
+using DJSCaseMgtService.DataAccess.Repositories;
+using DJSCaseMgtService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace EmpowerApi.Controllers
+{
+    [RoutePrefix("api/Document")]
+    public class DocumentController : BaseController<Document>
+    {
+        private DJSCaseMgtContext context = new DJSCaseMgtContext();
+        public DocumentController(IBaseRepository<Document> baseRepository) : base(baseRepository) { }
+     
+
+        [System.Web.Http.HttpGet, Route("GetAll")]
+        public IHttpActionResult GetAll()
+        {
+            int systemID = base.authRepository.GetSystemIDByLoggedInUserRole();
+
+            IEnumerable<Document> output = null;
+            if (ModelState.IsValid)
+            {
+                output = context.Document.Where(x => x.Active == true && x.SystemID == systemID).ToList();
+            }
+
+            return Ok(output);
+        }
+
+
+
+        [System.Web.Http.HttpGet, Route("Delete/{id:int}")]
+        public async Task<int> Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var record = context.Document.Where(x => x.ID == id).FirstOrDefault();
+                if (record != null)
+                {
+                    record.Active = false;
+                }
+                try
+                {
+                    context.SaveChanges();
+                    return 1;
+                }
+                catch (Exception ex)
+                {
+                    return 0;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
