d95786f29a6fadd3d44aa8d84b91576ef4243cf7
Craig Maclvor <Craig Maclvor@richmondgov.com>
2020-04-24 09:59:34 -0400

    add school controller

---
+++ EmpowerApi/Controllers/SchoolController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DJSCaseMgmtModel.Entities;
+using DJSCaseMgtService.Models;
+using DJSCaseMgtService.DataAccess.Repositories;
+using EmpowerApi.Controllers;
+
+namespace DJSCaseMgtService.Controllers
+{
+    [RoutePrefix("api/School")]
+    public class SchoolController : BaseController<School>
+
+    {
+        private DJSCaseMgtContext context = new DJSCaseMgtContext();
+        public SchoolController(IBaseRepository<School> baseRepository) : base(baseRepository) { }
+
+        #region "Service methods"
+
+        [System.Web.Http.HttpGet, Route("GetAll")]
+        public IHttpActionResult GetAll()
+        {
+            IEnumerable<School> output = null;
+            if (ModelState.IsValid)
+            {
+                output = context.School.Where(x => x.Active == true).OrderBy(x => x.Name).ToList();
+            }
+
+            return Ok(output);
+        }
+
+
+
+        [System.Web.Http.HttpGet, Route("Delete/{id:int}")]
+        public async Task<int> Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var record = context.School.Where(x => x.ID == id).FirstOrDefault();
+                if (record != null)
+                {
+                    record.Active = false;
+                }
+                try
+                {
+                    context.SaveChanges();
+                    return 1;
+                }
+                catch (Exception ex)
+                {
+                    return 0;
+                }
+            }
+
+            return 0;
+        }
+        #endregion
+    }
+}
+
+
+
+
