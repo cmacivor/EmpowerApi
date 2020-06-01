e24aaa102abbb7d4bb40b5bb60e729de7c77d786
Craig Maclvor <Craig Maclvor@richmondgov.com>
2020-06-01 13:06:25 -0400

    add enrollment controller

---
+++ EmpowerApi/Controllers/EnrollmentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DJSCaseMgtService.DataAccess.Repositories;
+using DJSCaseMgmtModel.Entities;
+using DJSCaseMgmtModel.ViewModels;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DJSCaseMgtService.Models;
+using EmpowerApi.Controllers;
+
+namespace DJSCaseMgtService.Controllers
+{
+    [RoutePrefix("api/Enrollment")]
+    public class EnrollmentController : BaseController<Enrollment>
+    {
+        private IEnrollmentRepository enrollmentRepository;
+        private DJSCaseMgtContext context = new DJSCaseMgtContext();
+        public EnrollmentController(IBaseRepository<Enrollment> baseRepository, IEnrollmentRepository enrollmentRepository)
+            : base(baseRepository)
+        {
+            this.enrollmentRepository = enrollmentRepository;
+        }
+        [System.Web.Http.HttpGet, Route("DeleteEnrollment/{id:int}")]
+        public string DeleteEnrollment(int id)
+        {
+            var enrollments = enrollmentRepository.DeleteEnrollment(id);
+
+            return enrollments;
+        }
+
+
+        [System.Web.Http.HttpPut, Route("UpdateEnrollment/{id:int}")]
+        public int UpdateEnrollment(Enrollment enrollment)
+        {
+            var original = context.Enrollment.Find(enrollment.ID);
+            try
+            {
+                if (original != null)
+                {
+                    context.Entry(original).CurrentValues.SetValues(enrollment);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return enrollment.ID;
+        }
+    }
+}
