90fd6d7989def91393a20e16674ba13b83394b5c
Craig Maclvor <Craig Maclvor@richmondgov.com>
2020-06-22 17:03:07 -0400

    employmen and action plan controllers

---
+++ EmpowerApi/Controllers/EmploymentPlanController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DJSCaseMgtService.DataAccess.Repositories;
+using DJSCaseMgmtModel.Entities;
+using DJSCaseMgmtModel.ViewModels;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DJSCaseMgtService.Models;
+using EmpowerApi.Controllers;
+
+namespace DJSCaseMgtService.Controllers
+{
+    [RoutePrefix("api/EmploymentPlan")]
+    public class EmploymentPlanController : BaseController<EmploymentPlan>
+    {
+        private IEmploymentPlanRepository employmentPlanRepository;
+        private DJSCaseMgtContext context = new DJSCaseMgtContext();
+        public EmploymentPlanController(IBaseRepository<EmploymentPlan> baseRepository, IEmploymentPlanRepository employmentPlanRepository)
+            : base(baseRepository)
+        {
+            this.employmentPlanRepository = employmentPlanRepository;
+        }
+        [System.Web.Http.HttpGet, Route("DeleteEmploymentPlan/{id:int}")]
+        public string DeleteEmploymentPlan(int id)
+        {
+            var employments = employmentPlanRepository.DeleteEmploymentPlan(id);
+
+            return employments;
+        }
+
+        [System.Web.Http.HttpPut, Route("UpdateEmploymentPlan/{id:int}")]
+        public int UpdateEmploymentPlan(EmploymentPlan employment)
+        {
+            var original = context.EmploymentPlan.Find(employment.ID);
+            try
+            {
+                if (original != null)
+                {
+                    context.Entry(original).CurrentValues.SetValues(employment);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return employment.ID;
+
+        }
+
+    }
+}
